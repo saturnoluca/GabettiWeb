95aa39fe5e4c0c7a5ead899834a5606c87cba6f1
saturnoluca <75441710+saturnoluca@users.noreply.github.com>
2022-03-06 16:36:11 +0100

    sium

--- src/main/java/control/appartamento/IndexServlet.java
+++ src/main/java/control/appartamento/IndexServlet.java
@@ -1,9 +1,12 @@
 package control.appartamento;
 
+import UtilityClass.CompositeKeyAgenteCase;
 import model.agente.AgenteBean;
 import model.agente.AgenteModelDM;
 import model.appartamento.AppartamentoBean;
 import model.appartamento.AppartamentoModelDM;
+import model.indirizzo.IndirizzoBean;
+import model.indirizzo.IndirizzoModelDM;
 import model.utente.UtenteBean;
 import model.utente.UtenteModelDM;
 
@@ -19,29 +22,38 @@ public class IndexServlet extends HttpServlet {
     private static AppartamentoModelDM modelApp = new AppartamentoModelDM();
     private static AgenteModelDM modelAgenti = new AgenteModelDM();
     private static UtenteModelDM modelUtente = new UtenteModelDM();
+    private static IndirizzoModelDM modelIndirizzo= new IndirizzoModelDM();
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ArrayList<AppartamentoBean> appartamenti = new ArrayList<AppartamentoBean>();
         ArrayList<AgenteBean> agenti = new ArrayList<AgenteBean>();
         ArrayList<UtenteBean> utente = new ArrayList<UtenteBean>();
+        ArrayList<IndirizzoBean> indirizzi = new ArrayList<IndirizzoBean>();
+        ArrayList<CompositeKeyAgenteCase> agenteCase = new ArrayList<CompositeKeyAgenteCase>();
 
         try {
             if (request.getSession().getAttribute("NoDbConnection") != null) {
                 modelApp = null;
                 modelAgenti = null;
                 modelUtente = null;
+                modelIndirizzo = null;
             }
             appartamenti = (ArrayList<AppartamentoBean>) modelApp.OrderByVisite();
             agenti = (ArrayList<AgenteBean>) modelAgenti.RetrieveAgente();
             utente = (ArrayList<UtenteBean>) modelUtente.doRetrieveAll();
+            indirizzi = (ArrayList<IndirizzoBean>) modelIndirizzo.RetrieveAll();
+            agenteCase = (ArrayList<CompositeKeyAgenteCase>) modelAgenti.RetrieveAgenteCase();
         } catch (Exception e) {
             e.printStackTrace();
         }
         request.setAttribute("utente", utente);
         request.setAttribute("agenti", agenti);
         request.setAttribute("appartamenti", appartamenti);
-        RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+        request.setAttribute("indirizzi", indirizzi);
+        request.setAttribute("agenteCase", agenteCase);
+        RequestDispatcher rd = request.getRequestDispatcher("/FinalIndex.jsp");
         rd.forward(request, response);
 
     }
---
+++ target/GabettiWeb-1.0-SNAPSHOT/script/index.js
@@ -0,0 +1,9 @@
+function advancedOption(){
+    if(document.getElementById("advanced_option_div").style.display == "none"){
+        $('#advanced_option_div').slideDown();
+    }
+    else{
+        $('#advanced_option_div').slideUp();
+    }
+
+}
